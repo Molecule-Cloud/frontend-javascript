2fbba346481afb5f3aef412fb5bc5efe31b0e674
Benjamin Appiah <ben.appiah451@gmail.com>
2025-07-11 04:00:25 +0000

    Corrections made

---
+++ 0x04-Typescript/task_1/main.js
@@ -0,0 +1,16 @@
+function printTeacher(teacher) {
+    return "".concat(teacher.firstName[0], ". ").concat(teacher.lastName);
+}
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
--- 0x04-Typescript/task_1/main.ts
+++ 0x04-Typescript/task_1/main.ts
@@ -10,3 +10,27 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number
 }
+
+
+function printTeacher(teacher: Teacher): string {
+    return `${teacher.firstName[0]}. ${teacher.lastName}`;
+}
+
+interface printTeacherFunction {}
+
+class StudentClass {
+    firstName: string
+    lastName: string
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
